3643f9ce6e18e68311bdaf590934b7ac99dd3f07
nenatesz <nnennaanyadike@gmail.com>
2022-03-11 08:25:11 -0800

    use printf to display to the standard output

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main - print a word to the standard output
+ * Return 0 if sucess, else return non-zero 
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
