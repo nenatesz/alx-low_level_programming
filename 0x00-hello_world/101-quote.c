20cb9a3cb6f505e8c6822c660c807dc5666e18ba
nenatesz <nnennaanyadike@gmail.com>
2022-03-13 05:38:20 -0700

    print file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - prints exactly,
+ * Return: 1 if (Success)
+*/
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora
+Korpar, 2015-10-19\n", 59, 1, stderr);
+	return (1);
+}
