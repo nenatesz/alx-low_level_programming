e6a7b70b420a72e43ba261428cfdce6fac42d936
nenatesz <nnennaanyadike@gmail.com>
2022-03-14 08:23:16 -0700

    fixed issues with the condtonals

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,16 +7,19 @@ int main(void)
 {
 	int n, m;
 
-	for (n = 0; n <= 9; n++)
+	for (n = 0; n <= 8; n++)
 	{
-		for (m = 0; m <= 8; m++)
+		for (m = 1; m <= 9; m++)
 		{
-			putchar(n + '0');
-			putchar(m + '0');
-			if (n != 9 || m != 8)
+			if (m != n)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(n + '0');
+				putchar(m + '0');
+				if ((n != 8) || (m != 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
