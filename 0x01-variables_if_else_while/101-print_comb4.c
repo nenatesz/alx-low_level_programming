ba5b1d5f9f2bbd237051b781430e8391efb41bcb
nenatesz <nnennaanyadike@gmail.com>
2022-03-14 16:50:47 -0700

    print combo

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 if successful
+*/
+int main(void)
+{
+	int l, m, n;
+
+	for (l = 0; l <= 7; l++)
+	{
+		for (m = l + 1; n <= 8; m++)
+		{
+			for (n = m + 1; n <= 9; n++)
+			{
+				if (l != m || m != n || l != n)
+				{
+					putchar(l + '0');
+					putchar(m + '0');
+					putchar(n + '0');
+					if ((l != 7) || (m != 8) || (n != 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+}
