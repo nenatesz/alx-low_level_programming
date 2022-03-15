3bedc4e2ce476e6aad75d08442accf7fd775fc0f
nenatesz <nnennaanyadike@gmail.com>
2022-03-15 15:38:56 -0700

    changes

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,6 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - entry point
- * Return: 0 if successful
-*/
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
-}
-
-/**
  * print_alphabet_x10 - entry point
 */
 void print_alphabet_x10(void)
