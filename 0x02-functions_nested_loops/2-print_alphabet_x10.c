aceb81d0e485c12823b237dc49c5eb84c032da2a
nenatesz <nnennaanyadike@gmail.com>
2022-03-15 15:46:25 -0700

    changes

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,7 +7,7 @@ void print_alphabet_x10(void)
 {
 	char i, j;
 
-	i = 0;
+	i = 0
 	while (i <= 10)
 	{
 		j = 'a';
@@ -17,6 +17,7 @@ void print_alphabet_x10(void)
 			_putchar(j);
 			j++;
 		}
+		_putchar('\n');
+		i++;
 	}
-	i++;
 }
