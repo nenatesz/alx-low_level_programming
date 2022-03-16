8a12010fa82b196fcc0a8cca711b8bfee8e60614
nenatesz <nnennaanyadike@gmail.com>
2022-03-15 18:25:03 -0700

    changes

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,14 +10,17 @@
 */
 int print_last_digit(int n)
 {
-	if ((n / 100) < 1)
-	{
-		_putchar(abs(n % 10) + '0');
-		return (abs(n % 10));
-	}
-	else if ((n / 100) >= 1)
-	{
-		return (abs(n % 100));
-	}
-	return (0);
+		int a;
+
+			if (n < 0)
+						n = -n;
+
+				a = n % 10;
+
+					if (a < 0)
+								a = -a;
+
+						_putchar(a + '0');
+
+							return (a);
 }
