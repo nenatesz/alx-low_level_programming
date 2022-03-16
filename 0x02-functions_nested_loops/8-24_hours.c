4e2646defb0d2499840f495b06cc9241362fa14c
nenatesz <nnennaanyadike@gmail.com>
2022-03-15 18:49:18 -0700

    changes

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * jack_bauer - main entry
+*/
+void jack_bauer(void)
+{
+	i, j, k, l;
+
+	i = 0;
+	while (i <= 2)
+	{
+		_putchar(i + '0');
+		j = 0;
+		while (j <= 3)
+		{
+			_putchar(j + '0');
+			_putchar(':');
+			k = 0;
+			while (k <= 5)
+			{
+				_putchar(k + '0');
+				l = 0;
+				while (l <= 9)
+				{
+					_putchar(l + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
