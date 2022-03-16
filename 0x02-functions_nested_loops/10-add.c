fc1e365a334d0afc8b9adc75f95305e6504e9193
nenatesz <nnennaanyadike@gmail.com>
2022-03-15 19:46:14 -0700

    add ints

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - entry point
+ * @i: parameter
+ * @j: parameter
+ * Return: 0 if successful
+*/
+int add(int i, int j)
+{
+	int r;
+
+	r = i + j;
+
+	return (r);
+}
