a3f2ae42309c97f139140eb8fba24fb218fe2835
nenatesz <nnennaanyadike@gmail.com>
2022-03-22 15:53:15 -0700

    changes

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * puts2 - entry point
+ * @str: pointer
+*/
+void puts2(char *str)
+{
+	int len, i;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+}
