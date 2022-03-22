d2d532e002a42ddba660561aabe249d9ac9274ea
nenatesz <nnennaanyadike@gmail.com>
2022-03-22 15:26:05 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.c"
+#include <string.h>
+/**
+ * rev_string - reverses a string
+ * @str: string to be reversed
+*/
+void rev_string(char *str)
+{
+	int len;
+	int i;
+
+	len = strlen(str) - 1;
+	for (i = 0; i <= len / 2 && str[i] != '\0'; i++)
+	{
+		str[i], str[len - i] = str[len - i], str[i];
+	}
+}
